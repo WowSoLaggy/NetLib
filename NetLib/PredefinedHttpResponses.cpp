f7f07e16dccb0771e31db1641a8d117565c1e24a
Anton Egorov <dohxehapo@gmail.com>
2016-08-04 15:45:41 +0300

    - centered page text

--- NetLib/PredefinedHttpResponses.cpp
+++ NetLib/PredefinedHttpResponses.cpp
@@ -26,7 +26,7 @@ namespace NetLib
 
 		std::string filePath = Config::GetRootFolder() + "/" + Config::GetDefaultPage400();
 		if (!CheckFileExists(filePath))
-			response.m_body = "<html><head><title>400 Bad Request</title><meta charset = \"utf-8\"/></head><body><h1>400 Bad Request</h1></body></html>";
+			response.m_body = "<html><head><title>400 Bad Request</title><meta charset = \"utf-8\"/></head><body><br><center><h1>400 Bad Request</h1></center></body></html>";
 		else
 			response.m_body = ReadFileToString(filePath);
 		response.m_headers.insert({ "Content-Length", std::to_string(response.m_body.size()) });
@@ -44,7 +44,7 @@ namespace NetLib
 
 		std::string filePath = Config::GetRootFolder() + "/" + Config::GetDefaultPage405();
 		if (!CheckFileExists(filePath))
-			response.m_body = "<html><head><title>405 Method Not Allowed</title><meta charset = \"utf-8\"/></head><body><h1>405 Method Not Allowed</h1></body></html>";
+			response.m_body = "<html><head><title>405 Method Not Allowed</title><meta charset = \"utf-8\"/></head><body><br><center><h1>405 Method Not Allowed</h1></center></body></html>";
 		else
 			response.m_body = ReadFileToString(filePath);
 		response.m_headers.insert({ "Content-Length", std::to_string(response.m_body.size()) });
@@ -62,7 +62,7 @@ namespace NetLib
 
 		std::string filePath = Config::GetRootFolder() + "/" + Config::GetDefaultPage501();
 		if (!CheckFileExists(filePath))
-			response.m_body = "<html><head><title>501 Method Not Implemented</title><meta charset = \"utf-8\"/></head><body><h1>501 Method Not Implemented</h1></body></html>";
+			response.m_body = "<html><head><title>501 Method Not Implemented</title><meta charset = \"utf-8\"/></head><body><br><center><h1>501 Method Not Implemented</h1></center></body></html>";
 		else
 			response.m_body = ReadFileToString(filePath);
 		response.m_headers.insert({ "Content-Length", std::to_string(response.m_body.size()) });
@@ -80,7 +80,7 @@ namespace NetLib
 
 		std::string filePath = Config::GetRootFolder() + "/" + Config::GetDefaultPage414();
 		if (!CheckFileExists(filePath))
-			response.m_body = "<html><head><title>414 URI Too Large</title><meta charset = \"utf-8\"/></head><body><h1>414 URI Too Large</h1></body></html>";
+			response.m_body = "<html><head><title>414 URI Too Large</title><meta charset = \"utf-8\"/></head><body><br><center><h1>414 URI Too Large</h1></center></body></html>";
 		else
 			response.m_body = ReadFileToString(filePath);
 		response.m_headers.insert({ "Content-Length", std::to_string(response.m_body.size()) });
@@ -98,7 +98,7 @@ namespace NetLib
 
 		std::string filePath = Config::GetRootFolder() + "/" + Config::GetDefaultPage404();
 		if (!CheckFileExists(filePath))
-			response.m_body = "<html><head><title>404 Not Found</title><meta charset = \"utf-8\"/></head><body><h1>404 Not Found</h1></body></html>";
+			response.m_body = "<html><head><title>404 Not Found</title><meta charset = \"utf-8\"/></head><body><br><center><h1>404 Not Found</h1></center></body></html>";
 		else
 			response.m_body = ReadFileToString(filePath);
 		response.m_headers.insert({ "Content-Length", std::to_string(response.m_body.size()) });
